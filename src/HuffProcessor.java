ac526d2a50bbf37ed3e4600132c223858444c4aa
Alysha Kamat <aak44@duke.edu>
2018-12-08 22:16:22 -0500

    asdg

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -9,7 +9,7 @@
  * 
  * @author Owen Astrachan
  */
-
+import java.util.*;
 public class HuffProcessor {
 
 	public static final int BITS_PER_WORD = 8;
@@ -41,14 +41,89 @@ public class HuffProcessor {
 	 *            Buffered bit stream writing to the output file.
 	 */
 	public void compress(BitInputStream in, BitOutputStream out){
-
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
-		}
+		int[] counts=readForCounts(in);
+		HuffNode root=makeTree(counts);
+		String[] codings=makeCodings(root);
+		out.writeBits(BITS_PER_INT, HUFF_TREE);
+		writeHeader(root,out);
+		in.reset();
+		writeCompressed(codings,in,out);
 		out.close();
 	}
+	private void writeCompressed(String[] codings, BitInputStream in, BitOutputStream out) {
+		// TODO Auto-generated method stub
+		while(true) {
+			int val= in.readBits(BITS_PER_WORD);
+			if(val==-1)break;
+			String code= codings[val];
+			out.writeBits(code.length(),Integer.parseInt(code,2));
+		}
+		String pe=codings[PSEUDO_EOF];
+		out.writeBits(pe.length(), Integer.parseInt(pe,2));
+	}
+
+	private void writeHeader(HuffNode root, BitOutputStream out) {
+		// TODO Auto-generated method stub
+		if(root.myLeft!=null && root.myRight!=null) {
+			out.writeBits(1,0);
+			writeHeader(root.myLeft,out);
+			writeHeader(root.myRight,out);
+		}
+		else {
+			out.writeBits(1, 1);
+			out.writeBits(BITS_PER_WORD+1, root.myValue);
+		}
+	}
+
+	private String[] makeCodings(HuffNode root) {
+		// TODO Auto-generated method stub
+		String[] code=new String[ALPH_SIZE +1];
+		changer(root,"",code);
+		return code;
+	}
+
+	private void changer(HuffNode root, String string, String[] code) {
+		// TODO Auto-generated method stub
+		if(root.myLeft==null && root.myRight==null) {
+			code[root.myValue]=string;
+			return ;
+		}
+		changer(root.myRight,string+"1",code);
+		changer(root.myLeft,string+"0",code);
+	}
+
+	private HuffNode makeTree(int[] counts) {
+		// TODO Auto-generated method stub
+		PriorityQueue<HuffNode> pq= new PriorityQueue<>();
+		for(int i=0;i<counts.length;i++) {
+			if(counts[i]>0) {
+				pq.add(new HuffNode(i,counts[i],null,null));
+			}
+		}
+		while(pq.size()>1) {
+			HuffNode left=pq.remove();
+			HuffNode right=pq.remove();
+			HuffNode a=new HuffNode(0,left.myWeight+right.myWeight,left,right);
+			pq.add(a);
+		}
+		HuffNode root=pq.remove();
+		return root;
+	}
+
+	private int[] readForCounts(BitInputStream in) {
+		// TODO Auto-generated method stub
+		int[] counts=new int[ALPH_SIZE + 1];
+		while(true) {
+			int countV=in.readBits(BITS_PER_WORD);
+			if(countV==-1) {
+				break;
+			}
+			counts[countV]++;
+		}
+		counts[PSEUDO_EOF]=1;
+		return counts;
+	}
+
 	/**
 	 * Decompresses a file. Output file must be identical bit-by-bit to the
 	 * original.
@@ -63,8 +138,54 @@ public class HuffProcessor {
 		while (true){
 			int val = in.readBits(BITS_PER_WORD);
 			if (val == -1) break;
+			if(val != HUFF_TREE || val==-1) {
+				throw new HuffException("Illegal header starts with" +val);
+			}
+			HuffNode root=readHeader(in);
+			readCompressed(root,in,out);
 			out.writeBits(BITS_PER_WORD, val);
 		}
 		out.close();
+	}
+
+	private void readCompressed(HuffNode root, BitInputStream in, BitOutputStream out) {
+		// TODO Auto-generated method stub
+		HuffNode curr=root; 
+		while(true) {
+			int val=in.readBits(1);
+			if(val==-1) {
+				throw new HuffException("no PSEUDO_EOF");
+			}
+			if(val==0) {
+				curr=curr.myLeft;
+			}
+			else {
+				curr=curr.myRight;
+				if(curr.myLeft==null && curr.myRight==null) {
+					if(curr.myValue==PSEUDO_EOF) {
+						break;
+					}
+					else {
+						out.writeBits(BITS_PER_WORD, curr.myValue);
+						curr=root;
+					}
+				}
+			}
+		}
+	}
+
+	private HuffNode readHeader(BitInputStream in) {
+		// TODO Auto-generated method stub
+		int val =in.readBits(1);
+		if(val==-1) {
+			throw new HuffException("Illegal header starts with" +val);
+		}
+		if(val==0) {
+			HuffNode left=readHeader(in);
+			HuffNode right=readHeader(in);
+			return new HuffNode(0,0,left,right);
+		}
+		val=in.readBits(BITS_PER_WORD+1);
+		return new HuffNode(val,0,null,null);
 	}
 }
